1fa718e7476c6d0b73fa1697030f844609f2cadd
jeremi gendron <gendronjeremi@gmail.com>
2018-08-23 03:36:20 +0000

    load transactions into blocks

--- packages/core-graphql/__tests__/graphql.test.js
+++ packages/core-graphql/__tests__/graphql.test.js
@@ -3,11 +3,27 @@ const app = require('./__support__/setup')
 let graphql
 let logger
 
+<<<<<<< Updated upstream
 const { testQueries,
         testData,
         testExpected,
         testHistory
+=======
+const { testData,
+        testQueries,
+        testExpected
+>>>>>>> Stashed changes
       } = require('./__fixtures__')
+
+let references = {
+  data: {
+    blocks: [],
+    transactions: [],
+    wallets: []
+  },
+  queries: {},
+  expected: {}
+}
 
 beforeAll(async () => {
   const container = await app.setUp()
@@ -25,18 +41,37 @@ describe('GraphQL', () => {
     expect(graphql).toBeObject()
   })
 
+  describe('test data', () => {
+    it('should be an object', () => {
+      expect(testData).toBeObject()
+    })
+
+    describe('loading blocks', () => {
+      references.data.blocks = testData.blocks
+      expect(references.data.blocks).toBe(testData.blocks)
+
+      it('should load transactions', () => {
+        for(let i = 0; i < testData.transactions.length; i++){
+          let transaction = testData.transactions[i]
+          let blockIndex = references.data.blocks.findIndex(block => block.id == transaction.block)
+
+          references.data.blocks[blockIndex].transactions.push(transaction)
+
+          logger.info(`Block with id ${references.data.blocks[i].id} now has transaction with id ${testData.transactions[i].id}`)
+        }
+      })
+
+      it('should now have transactions', () => {
+        expect(testData.transactions).toEqual(expect.arrayContaining(references.data.blocks[0].transactions))
+      })
+    })
+  })
+
   describe('test queries', () => {
     it('should be an object', () => {
       expect(testQueries).toBeObject()
     })
   })
-
-  describe('test data', () => {
-    it('should be an object', () => {
-      expect(testData).toBeObject()
-    })
-  })
-
   describe('test expected', () => {
     it('should be an object', () => {
       expect(testExpected).toBeObject()
